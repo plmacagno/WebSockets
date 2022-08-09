7b0e847e6ffb1d5f079209d72e192e27b3f0c819
Pablo Macagno <plmacagno@gmail.com>
2022-08-09 07:33:34 -0300

    Arreglos

--- views/index.js
+++ views/index.js
@@ -43,11 +43,9 @@ socket.on("connect", () => {
   });
   
   socket.on("UPDATE_MESSAGES", (msg, allMessages) => {
-	// alert(msg);
+	
 	document.getElementById("posts").innerHTML = "";
-	// for (let msg of allMessages) {
-	//   appendMessage(msg);
-	// }
+	
 	allMessages
 	  .sort((a,b) => a.date - b.date)
 	  .forEach(msg => appendMessage(msg));
@@ -78,39 +76,3 @@ socket.on("connect", () => {
 	socket.emit("LIKE_MESSAGE", msgId);
   }
 
-// Chat form
-/*const $chatForm = document.querySelector('#chat-form');
-const $userEmail = document.querySelector('#user-email');
-const $chatMessage = document.querySelector('#chat-message');
-const $tableChat = document.querySelector('#table-chat');
-
-$chatForm.addEventListener('submit', e => {
-	e.preventDefault();
-	if ($userEmail.value == '') return alert('Ingresa tu email');
-	const newMessage = {
-		userEmail: $userEmail.value,
-		message: $chatMessage.value,
-		date: new Date().toLocaleString()
-	}
-	socket.emit('newMessage', newMessage);
-	e.target.reset();
-});
-
-const renderChat = messages => {
-	if (messages.length > 0) $tableChat.innerHTML = '';
-	messages.forEach(message => {
-		$tableChat.innerHTML += `
-		<div>
-			<b class="text-primary">${message.userEmail}</b>
-			[<span style="color: brown;">${message.date}</span>]
-			: <i class="text-success">${message.message}</i>
-		</div > `;
-	})
-	$chatMessage.focus();
-}
-
-
-
-socket.on('messages', messages => {
-	renderChat(messages);
-});*/
